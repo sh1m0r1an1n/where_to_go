170b023a993de42a6f93672bbf39a179744d8e2b
Egor Akhmanaev <akhmanaev2021@gmail.com>
2025-07-12 18:37:36 +0500

    Add Place and PlaceImage models

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Place, PlaceImage
 
-# Register your models here.
+
+class PlaceImageInline(admin.TabularInline):
+    """Inline для отображения изображений места в админке"""
+    
+    model = PlaceImage
+    extra = 1
+    fields = ['image_url']
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    """Админка для модели Place"""
+    
+    list_display = [
+        'title', 
+        'latitude', 
+        'longitude',
+        'images_count'
+    ]
+    
+    search_fields = ['title', 'description_short', 'description_long']
+    
+    inlines = [PlaceImageInline]
+    
+    def images_count(self, obj):
+        """Показывает количество изображений для места"""
+        count = obj.images.count()
+        return f"{count} изображений"
+    images_count.short_description = "Изображения"
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    """Админка для модели PlaceImage"""
+    
+    list_display = [
+        'place', 
+        'image_preview'
+    ]
+    
+    list_filter = ['place']
+    
+    search_fields = ['place__title', 'image_url']
+    
+    ordering = ['place', 'id']
+    
+    def image_preview(self, obj):
+        """Показывает превью изображения"""
+        if obj.image_url:
+            return format_html(
+                '<img src="{}" style="max-height: 50px; max-width: 100px;" />',
+                obj.image_url
+            )
+        return "Нет изображения"
+    image_preview.short_description = "Превью"
--- places/models.py
+++ places/models.py
@@ -1,4 +1,59 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
-print("hello!")
+
+class Place(models.Model):
+    """Модель для хранения информации о местах"""
+    
+    title = models.CharField(max_length=200, verbose_name="Название места")
+    description_short = models.TextField(verbose_name="Краткое описание")
+    description_long = models.TextField(verbose_name="Подробное описание")
+    
+    latitude = models.DecimalField(
+        max_digits=16,
+        decimal_places=14,
+        validators=[
+            MinValueValidator(-90),
+            MaxValueValidator(90)
+        ],
+        verbose_name="Широта"
+    )
+    
+    longitude = models.DecimalField(
+        max_digits=16,
+        decimal_places=14,
+        validators=[
+            MinValueValidator(-180),
+            MaxValueValidator(180)
+        ],
+        verbose_name="Долгота"
+    )
+    
+    class Meta:
+        verbose_name = "Место"
+        verbose_name_plural = "Места"
+        ordering = ['title']
+    
+    def __str__(self):
+        return self.title
+
+
+class PlaceImage(models.Model):
+    """Модель для хранения изображений мест"""
+    
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        related_name='images',
+        verbose_name="Место"
+    )
+    
+    image_url = models.URLField(max_length=500, verbose_name="URL изображения")
+    
+    class Meta:
+        verbose_name = "Изображение места"
+        verbose_name_plural = "Изображения мест"
+        ordering = ['place', 'id']
+    
+    def __str__(self):
+        return f"Изображение для {self.place.title}"
